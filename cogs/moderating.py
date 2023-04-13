c44bf0a62cc63e90981d8533087dceb61b95eaeb
Nat1an <nathan@nat1an.xyz>
2023-04-13 22:55:52 +0800

    🐛 fix(moderating.py): use 'not in' instead of '!=' to check if author is bot owner ✨ feat(moderating.py): add support for user_id parameter in reset command ✨ feat(moderating.py): add support for ephemeral parameter in delete and edit commands

--- cogs/moderating.py
+++ cogs/moderating.py
@@ -44,11 +44,11 @@ class Moderating(commands.Cog):
 
     )
     async def eval(self, interaction: ApplicationCommandInteraction, code: str, ephemeral: bool = True):
-        if not interaction.author.id == self.bot.owner_ids:
+        if interaction.author.id not in self.bot.owner_ids:
             return await interaction.response.send_message("❌ 你不是我的主人，你不能這麼做", ephemeral=ephemeral)
 
         await interaction.response.defer(ephemeral=ephemeral)
-        
+
         try:
             import asyncio
             result = eval(code)
@@ -85,7 +85,7 @@ class Moderating(commands.Cog):
     )
     async def reset(self, interaction: ApplicationCommandInteraction,
                     user: User = None, user_id: str = None, ephemeral: bool = True):
-        if not interaction.author.id in self.bot.owner_ids:
+        if interaction.author.id not in self.bot.owner_ids:
             return await interaction.response.send_message("❌ 你不是我的主人，你不能這麼做", ephemeral=ephemeral)
 
         await interaction.response.defer(ephemeral=ephemeral)
@@ -108,7 +108,7 @@ class Moderating(commands.Cog):
 
     @commands.message_command(name="刪除", description="刪除初春的訊息")
     async def delete(self, interaction: MessageCommandInteraction):
-        if not interaction.author.id == self.bot.owner_ids:
+        if interaction.author.id not in self.bot.owner_ids:
             return await interaction.response.send_message("❌ 你不是我的主人，你不能這麼做", ephemeral=True)
 
         await interaction.target.delete()
@@ -116,7 +116,7 @@ class Moderating(commands.Cog):
 
     @commands.message_command(name="編輯", description="編輯初春的訊息")
     async def edit(self, interaction: MessageCommandInteraction):
-        if not interaction.author.id in self.bot.owner_ids:
+        if interaction.author.id not in self.bot.owner_ids:
             return await interaction.response.send_message("❌ 你不是我的主人，你不能這麼做", ephemeral=True)
 
         await interaction.response.send_modal(
